68f8776af85efd42be6cbde3bc96ab4a94e2461a
Jose Garcia <josegarciamanez@gmail.com>
2018-04-24 10:21:04 +0200

    heroku conf

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const hbs = require('hbs')
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000;
+
 app.use(express.static(__dirname + '/public'))
     // Express HBS engine
 hbs.registerPartials(__dirname + '/views/parciales')
@@ -18,6 +20,6 @@ app.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.listen(3000, () => {
-    console.log('Escuchando Peticiones en el puerto 3000')
+app.listen(port, () => {
+    console.log(`Escuchando Peticiones en el puerto ${port}`)
 })
